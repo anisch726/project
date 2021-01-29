a41f6c3dba23684d23355097fd949c4fc399a1a4
unknown <chaouachi508@gmail.com>
2021-01-29 14:25:44 +0200

    aaa

---
+++ src/app/admin/formateurs.ts
@@ -0,0 +1,27 @@
+import { Formateur } from './formateur';
+export const FORMATEURITEMS: Formateur[] = [
+    {
+     id: 1111,
+     nom: "Bouzidi",
+     prenom: "Amine",
+     Adresse: "Manouba",
+     Email: "amin00@gmail.com",
+     Domaine: "Data Science"
+    },
+    {
+        id: 2,
+        nom: "Chaouachi",
+        prenom: "Anis",
+        Adresse: "tunis",
+        Email: "chaouachi508@gmail.com",
+        Domaine: "AI"
+       },
+       {
+        id: 3,
+        nom: "Walid",
+        prenom: "Saad",
+        Adresse: "tunis",
+        Email: "walidsaad2010@gmail.com",
+        Domaine: "web"
+       }
+];
---
+++ src/app/admin/sessions.ts
@@ -0,0 +1,33 @@
+import { Session } from './session';
+export const SESSIONITEMS: Session[] = [
+{
+    id: 1,
+name: 'Web',
+track: 'MEAN',
+date: new Date('2021-06-19'),
+duree: 12,
+adress: 'Tunis',
+participants: 0,
+isCompleted : false
+},
+{
+id: 2,
+name: 'Mobile',
+track: 'Android',
+date: new Date('2021-07-19'),
+duree: 5,
+adress: 'tunis',
+participants: 0,
+isCompleted : false
+},
+{
+id: 3,
+name: 'Web',
+track: 'Angular',
+date: new Date('2021-08/19'),
+duree: 10,
+adress: 'Tunis',
+participants: 0,
+isCompleted : false
+}
+];
